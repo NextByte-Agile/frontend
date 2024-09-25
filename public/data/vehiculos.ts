af325c9fa1ee4d3052c29521439cb67c2aa81382
Jherico <jhericof.sv.189@gmail.com>
2024-09-24 21:04:05 -0500

    add vehiculos fotos

--- public/data/vehiculos.ts
+++ public/data/vehiculos.ts
@@ -4,18 +4,34 @@ export interface Vehiculo {
   brand: string;
   model: string;
   year: number;
+  img: string;
 }
 
 const vehiculos: Vehiculo[] = [
-  { id: 1, plate: "ABC-123", brand: "DFSK", model: "K07S", year: 2020 },
+  {
+    id: 1,
+    plate: "ABC-123",
+    brand: "DFSK",
+    model: "K07S",
+    year: 2020,
+    img: "/images/vehiculos/DFSK_K07S.jpg",
+  },
   {
     id: 2,
     plate: "XYZ-456",
     brand: "Changan",
     model: "Grand Supervan",
     year: 2019,
+    img: "/images/vehiculos/Changan_Grand_Supervan.jpg",
   },
-  { id: 3, plate: "QWE-789", brand: "Nissan", model: "Elgrand", year: 2018 },
+  {
+    id: 3,
+    plate: "QWE-789",
+    brand: "Nissan",
+    model: "Elgrand",
+    year: 2018,
+    img: "/images/vehiculos/Nissan_Elgrand.jpg",
+  },
 ];
 
 export default vehiculos;
--- src/components/VehicleTable.tsx
+++ src/components/VehicleTable.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import vehiculos from "/public/data/vehiculos";
 
 const VehicleTable = () => {
@@ -5,6 +6,7 @@ const VehicleTable = () => {
     <table className="w-full bg-white text-black shadow-md rounded-md">
       <thead>
         <tr className="bg-gray-200 uppercase text-sm">
+          <th className="py-2 px-4">Foto</th>
           <th className="py-2 px-4">Placa</th>
           <th className="py-2 px-4">Marca</th>
           <th className="py-2 px-4">Modelo</th>
@@ -14,6 +16,14 @@ const VehicleTable = () => {
       <tbody>
         {vehiculos.map((vehicle, index) => (
           <tr key={index} className="border-b text-center">
+            <td className="py-2 px-4 flex justify-center">
+              <Image
+                src={vehicle.img}
+                width={100}
+                height={100}
+                alt={vehicle.brand + " " + vehicle.model}
+              ></Image>
+            </td>
             <td className="py-2 px-4">{vehicle.plate}</td>
             <td className="py-2 px-4">{vehicle.brand}</td>
             <td className="py-2 px-4">{vehicle.model}</td>
