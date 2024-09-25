cd1166a6df1545ebf313f7f2d2e64534994fef61
Jherico <jhericof.sv.189@gmail.com>
2024-09-24 22:30:44 -0500

    max width

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,7 +17,8 @@ const Navigation = ({ children }: NavigationProps) => {
       {isAuthenticatedRoute && (
         <div className="flex h-screen">
           <Sidebar />
-          <div className="flex-1 ml-64 p-6">{children}</div>
+          <div className="flex-1 ml-64 p-6">
+            <div className="max-w-6xl mx-auto">{children}</div></div>
         </div>
       )}
       {!isAuthenticatedRoute && children}
